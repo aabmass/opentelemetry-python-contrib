03c9b74282a29e3f053a5102bb29078ec6e4f128
Aaron Abbott <aaronabbott@google.com>
2025-02-12 06:03:27 +0000

    Show the trace ID inline with messages

--- instrumentation-genai/opentelemetry-instrumentation-vertexai/examples/langgraph-chatbot-demo/src/langgraph_chatbot_demo/langchain_history.py
+++ instrumentation-genai/opentelemetry-instrumentation-vertexai/examples/langgraph-chatbot-demo/src/langgraph_chatbot_demo/langchain_history.py
@@ -1,5 +1,6 @@
 """Adapted from https://github.com/langchain-ai/streamlit-agent/blob/main/streamlit_agent/basic_memory.py"""
 
+import sqlite3
 import tempfile
 from os import environ
 from random import getrandbits
@@ -7,7 +8,7 @@ from random import getrandbits
 import streamlit as st
 from langchain_community.agent_toolkits.sql.toolkit import SQLDatabaseToolkit
 from langchain_community.utilities.sql_database import SQLDatabase
-from langchain_core.messages import SystemMessage
+from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.messages.base import BaseMessage
 from langchain_core.runnables.config import (
     RunnableConfig,
@@ -35,6 +36,7 @@ tracer = trace.get_tracer(__name__)
 title = "LangGraph SQL Agent Demo"
 st.set_page_config(page_title=title, page_icon="📖", layout="wide")
 st.title(title)
+_streamlit_helpers.styles()
 
 
 model = ChatVertexAI(
@@ -75,12 +77,19 @@ def search(query: str):
 
 
 system_prompt = SystemMessage(
-    content="You are a helpful AI assistant with a mastery of database management and querying. You have "
-    "access to an ephemeral sqlite database that you can query and modify through some tools. Help "
-    "answer questions and perform actions."
-    "\n"
-    "Make sure you always use QuerySQLCheckerTool to validate queries before running them! If you "
-    "make a mistake, try to recover."
+    content=f"""\
+You are a careful and helpful AI assistant with a mastery of database design and querying. You
+have access to an ephemeral sqlite3 database that you can query and modify through some tools.
+Help answer questions and perform actions. Follow these rules:
+
+- Make sure you always use sql_db_query_checker to validate SQL statements **before** running
+  them! In pseudocode: `checked_query = sql_db_query_checker(query);
+  sql_db_query(checked_query)`.
+- The sqlite version is {sqlite3.sqlite_version} which supports multiple row inserts.
+- Always prefer to insert multiple rows in a single call to the sql_db_query tool, if possible.
+- You may request to execute multiple sql_db_query tool calls which will be run in parallel.
+
+If you make a mistake, try to recover."""
 )
 
 
@@ -119,35 +128,49 @@ if checkpoint := checkpointer.get(config):
 else:
     messages = []
 
+
+@st.cache_resource
+def get_trace_ids(thread_id: str) -> "dict[str, str]":
+    # Stores the trace IDs. Unfortunately I can't find a way to easily retrieve this from the
+    # checkpointer, so just store it separately.
+    return {}
+
+
+trace_ids = get_trace_ids(st.query_params.thread_id)
+
 col1, col2 = st.columns([0.6, 0.4])
 with col1:
     _streamlit_helpers.render_intro()
     st.divider()
 
     # Add system message
-    st.chat_message(
-        "human", avatar=":material/precision_manufacturing:"
-    ).markdown(f"**System Instructions**\n> {system_prompt.content}")
+    st.expander(
+        "System Instructions", icon=":material/precision_manufacturing:"
+    ).markdown(system_prompt.content)
 
     # Render current messages
-    for msg in messages:
-        # Filter out tool calls
-        if msg.type in ("human", "ai") and msg.content:
-            st.chat_message(msg.type).write(msg.content)
+    for message in messages:
+        trace_id = trace_ids.get(message.id or "")
+        _streamlit_helpers.render_message(message, trace_id)
 
 # If user inputs a new prompt, generate and draw a new response
 if prompt := st.chat_input():
+    message = HumanMessage(prompt)
     with col1:
-        st.chat_message("human").write(prompt)
         with tracer.start_as_current_span(
             "chain invoke",
             attributes={"thread_id": st.query_params.thread_id},
-        ) as span, st.spinner("Thinking..."):
-            st.toast(
-                f"Trace ID {format_trace_id(span.get_span_context().trace_id)}"
-            )
+        ) as span:
+            trace_id = format_trace_id(span.get_span_context().trace_id)
+            _streamlit_helpers.render_message(message, trace_id=trace_id)
+
             # Invoke the agent
-            app.invoke({"messages": [prompt]}, config=config)
+            with st.spinner("Thinking..."):
+                res = app.invoke({"messages": [message]}, config=config)
+
+            # Store trace ID for rendering
+            trace_ids[message.id or ""] = trace_id
+            trace_ids[res["messages"][-1].id] = trace_id
 
     st.rerun()
 
@@ -160,6 +183,3 @@ with col2:
 
     with st.expander("View the message contents in session state"):
         st.json(messages)
-
-# https://pantheon.corp.google.com/traces/explorer;traceId=8a69802b84077b162277f175e0f15276;duration=PT30M?project=otlp-test-deleteme
-# https://pantheon.corp.google.com/traces/explorer;query=%7B%22plotType%22:%22HEATMAP%22,%22targetAxis%22:%22Y1%22,%22traceQuery%22:%7B%22resourceContainer%22:%22projects%2Fotlp-test-deleteme%2Flocations%2Fglobal%2FtraceScopes%2F_Default%22,%22spanDataValue%22:%22SPAN_DURATION%22,%22spanFilters%22:%7B%22attributes%22:%5B%5D,%22displayNames%22:%5B%22chain%20invoke%22%5D,%22isRootSpan%22:true,%22kinds%22:%5B%5D,%22maxDuration%22:%22%22,%22minDuration%22:%22%22,%22services%22:%5B%5D,%22status%22:%5B%5D%7D%7D%7D;traceId=8a69802b84077b162277f175e0f15276;duration=PT30M
